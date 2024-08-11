9ed1a7a52f4b5f6c2203c7bd6fe0a504b1620af1
hisham24 <hisham.tr24@gmail.com>
2024-08-11 10:35:59 +0100

    Initial setup of lsp-mode

--- emacs/init.el
+++ emacs/init.el
@@ -25,6 +25,12 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Set up environment variables, needed to get npm (from nvm) working
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
 (use-package rainbow-delimiters
  :hook (prog-mode . rainbow-delimiters-mode))
 
@@ -63,13 +69,60 @@
 
 (fido-mode t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; LSP Mode
+(defun efs/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :hook (lsp-mode . efs/lsp-mode-setup)
+  :commands (lsp lsp-deferred)
+  :init
+  (setq lsp-keymap-prefix "C-c l") ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+  :custom
+  (lsp-ui-doc-position 'bottom)
+  (lsp-ui-sideline-show-code-actions t))
+  ;:init
+  ;(setq lsp-ui-sideline-show-code-actions t))
+
+;; Typescript
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
+;; Company Mode (Better Completions UI for LSP)
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+	      ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+	      ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(gnu-elpa-keyring-update use_package)))
- '(package-selected-packages '(which-key doom-modeline rainbow-delimiters))
+ '(package-selected-packages
+   '(lsp-ui company-box company exec-path-from-shell typescript-mode lsp-mode gnu-elpa-keyring-update use_package which-key doom-modeline rainbow-delimiters))
  '(tab-bar-history-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -78,3 +131,4 @@
  ;; If there is more than one, they won't work right.
  )
 
+ 
